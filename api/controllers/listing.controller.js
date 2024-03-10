7d6d12352d709b17ba2b6b17c9a576f65bee8796
r4j1025 <raj2505000@gmail.com>
2024-03-10 12:04:07 +0530

    create search API route.

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -61,3 +61,50 @@ export const updateListing = async (req, res, next) => {
       next(error);
     }
   }
+
+  export const getListings = async (req,res,next)=>{
+    try {
+      const limit = parseInt(req.query.limit) || 9;   //http://localhost:3000/api/listing/get?limit=2  <-- this is the 'req.query.limit'
+      // this line says if there is a limit then parse it into limit variable or assign 9.
+      const startIndex = parseInt(req.query.startIndex) || 0;
+
+      let offer = req.query.offer; //true or false, if not mentioned in query then it is 'undefined'.
+      if (offer === undefined || offer === 'false'){
+        offer = {$in:[false,true]}; //if we not checked offer option then search 'in' db for both true and false values of offer.
+      }
+
+      let furnished = req.query.furnished; 
+      if (furnished === undefined || furnished === 'false'){
+        furnished = {$in:[false,true]}; 
+      }
+
+      let parking = req.query.parking; 
+      if (parking === undefined || parking === 'false'){
+        parking = {$in:[false,true]}; 
+      }
+
+      let type = req.query.type; 
+      if (type === undefined || type === 'all'){
+        type = {$in:['sale','rent']}; 
+      }
+
+      const searchTerm = req.query.searchTerm || '';
+
+      const sort = req.query.sort || 'createdAt';
+
+      const order = req.query.order || 'desc';
+
+      const listings = await Listing.find({  //regex is the builtin search func, it search for all possibilities
+        name: {$regex:searchTerm, $options:'i'}, // 'i' means search is both upper and lower case.
+        offer,
+        furnished,
+        parking,
+        type,
+      }).sort({[sort]:order}).limit(limit).skip(startIndex);  //use descending order for any type of sort , default: createdAt in desc order.
+
+      return res.status(200).json(listings);
+
+    } catch (error) {
+      next(error);
+    }
+  }
