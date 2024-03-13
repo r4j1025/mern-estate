7fe39f9e982069a9b0226dc110349e8aabb47987
r4j1025 <raj2505000@gmail.com>
2024-03-13 22:13:24 +0530

    render problem 2

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ListingItem from "../components/Listingitem";
+import ListingItem from "../components/ListingItem";
 
 
 const Search = () => {
