00c0171ff43fef899441579587fce40414a65efd
r4j1025 <raj2505000@gmail.com>
2024-03-13 12:21:40 +0530

    Create the listing item components and show listings.

---
+++ client/src/components/ListingItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {MdLocationOn} from 'react-icons/md';
+//.toLocaleString('en-US')  to add comma for price values.
+
+const ListingItem = ({listing}) => {
+  return (
+    <div className='bg-white w-full sm:w-[330px] shadow-md hover:shadow-lg rounded-lg overflow-hidden transition-shadow'>
+        <Link to={`/listing/${listing._id}`}>
+            <img className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300' src={listing.imageUrls[0] || "https://media.assettype.com/outlookbusiness%2F2023-10%2Fb0d05f17-546c-4c46-b85d-f7e1129068f4%2F10_Companies_That_Hire_for_Remote_Real_Estate_Jobs.jpg"} alt='listing cover' />
+        <div className='p-3 flex flex-col gap-2 w-full'>
+            <p className='text-lg border-b-2  border-slate-200 font-semibold truncate text-slate-700 '>{listing.name}</p>
+            <div className='flex  items-center gap-1'>
+                <MdLocationOn
+                    className='h-4 w-4 text-green-700 '
+                />
+                <p className='truncate w-full text-sm text-gray-600'>{listing.address}</p>
+            </div>
+            <p className='text-sm text-gray-600 line-clamp-2'>{listing.description}</p>
+            <p className=' mt-2 font-semibold  text-slate-500'>₹{listing.offer ? listing.discountPrice.toLocaleString('en-US') : listing.regularPrice.toLocaleString('en-US')}
+            {' '}
+                {listing.type === "rent" && '/ month'}
+            </p>
+            <div className= 'flex gap-4 text-slate-700'>
+                <div className='font-bold text-xs'>{listing.bedrooms >1 ? `${listing.bedrooms} beds` : `${listing.bedrooms} bed` }</div>
+                <div className='font-bold text-xs'>{listing.bathrooms >1 ? `${listing.bathrooms} bathrooms` : `${listing.bathrooms} bathroom` }</div>
+            </div>
+        </div>
+        </Link>
+    </div>
+  )
+}
+
+export default ListingItem
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "../components/Listingitem";
+
 
 const Search = () => {
   const [sidebardata, setSidebardata] = useState({
@@ -13,8 +15,6 @@ const Search = () => {
   });
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  console.log(listings)
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -209,10 +209,26 @@ const Search = () => {
           </button>
         </form>
       </div>
-      <div>
+      <div className="flex-1">
         <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
           Listing results:
         </h1>
+        <div className="p-3 flex flex-wrap gap-4">
+          {!loading && listings.length=== 0 && (
+            <p className="text-slate-700 text-xl">No listing found!</p>
+          )}
+          {loading && (
+            <p className="text-lg text-slate-700 text-center w-full">Loading...</p>
+          )}
+          {
+            !loading && listings && listings.map((listing)=>
+              <ListingItem
+                key={listing._id}
+                listing={listing}
+              />
+            )
+          }
+        </div>
       </div>
     </div>
   );
